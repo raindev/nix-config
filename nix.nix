1b07e4ba2de52010eae8ab6b2cdf310efaddfb14
Andrew Barchuk <andrew@raindev.io>
2022-12-19 21:40:06 +0100

    Retrigger nix GC & optimization if machine was off

--- nix.nix
+++ nix.nix
@@ -6,10 +6,16 @@
   nix.gc = {
     # remove unused files from nix storage
     automatic = true;
+    # catch up on runs missed due to machine being powered off
+    persistent = true;
     # remove obsolete generations
     options = "--delete-older-than 30d";
   };
   # run hardlink deduplication for nix store every night
   nix.optimise.automatic = true;
+  # catch up on runs missed due to machine being powered off
+  systemd.timers.nix-optimise.timerConfig = {
+    Persistent = true;
+  };
 
 }
