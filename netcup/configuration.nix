319cdd24f3ee4d96bc6e2365fccc10f29e325f02
Andrew Barchuk <andrew@raindev.io>
2023-01-28 19:31:48 +0100

    Install Rust on netcup

--- netcup/configuration.nix
+++ netcup/configuration.nix
@@ -76,12 +76,11 @@
     extraGroups = [ "wheel" ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    rustup
+    # rustc relies on cc linker
+    gcc
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
