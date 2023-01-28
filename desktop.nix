23caf261f555951a4c8f950147e3e068431b8f13
Andrew Barchuk <andrew@raindev.io>
2023-01-28 23:09:31 +0100

    Add GNOME extension to automate dark mode

--- desktop.nix
+++ desktop.nix
@@ -1,6 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # enable local network hostname resolution without relying on DNS
   services.avahi.nssmdns = true;
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.night-theme-switcher
+  ];
 }
