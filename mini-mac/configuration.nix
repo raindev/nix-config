b8118bd4fcfa9bec976dfaf82d36f94ac2b4932a
Andrew Barchuk <andrew@raindev.io>
2023-03-08 18:55:10 +0100

    Add macOS user configuration
    And configure the shell with Nix

--- mini-mac/configuration.nix
+++ mini-mac/configuration.nix
@@ -1,14 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    rustup
-  ];
-
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   # run Nix GC monthly
   nix.gc.interval.Day = 1;
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+  # /etc/shells
+  environment.shells = [ pkgs.bashInteractive ];
+
+  environment.systemPackages = with pkgs; [
+    rustup
+  ];
+
+  users.knownUsers = [ "raindev" ];
+  # the configuration will not be updated for already existing user
+  users.users.raindev = {
+    description = "Andrew Barchuk";
+    uid = 501;
+    gid = 20;
+    home = "/Users/raindev";
+    createHome = true;
+    # chsh
+    shell = "${pkgs.bashInteractive}/bin/bash";
+  };
 
   system.stateVersion = 4;
 }
