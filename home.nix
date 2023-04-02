acf6a4f09d44e1530ffcc902589cc295bcd5be06
Andrew Barchuk <andrew@raindev.io>
2023-04-03 00:44:37 +0200

    Move the common configuration files to chezmoi

--- home.nix
+++ home.nix
@@ -11,30 +11,6 @@
     hashKnownHosts = true;
     extraConfig = "AddKeysToAgent yes";
   };
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-    userName = "Andrew Barchuk";
-    userEmail = "andrew@raindev.io";
-    signing.key = "B14FD6B79497FC060E88564D548706C0A2EEA361";
-    signing.signByDefault = true;
-    aliases = {
-      continue = "!f() { git commit --edit --file=$(git rev-parse --git-dir)/COMMIT_EDITMSG; }; f";
-    };
-    ignores = [ "*~" "*.swp" "*.swo" ];
-    extraConfig = {
-      init.defaultBranch = "main";
-      pull.ff = "only";
-      push.autoSetupRemote = true;
-    };
-  };
-
-  home.activation.wezterm = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ln -sf ~/code/nix-config/home/wezterm.lua ~/.wezterm.lua
-  '';
-  home.file.".bash_functions".source = home/bash_functions;
-  xdg.configFile."nvim".source = home/nvim;
-  home.file.".curlrc".text = ''--write-out "\n"'';
 
   home.stateVersion = "22.11";
 }
